cec58173d2bf5b050b430005591edc11bf1167bb
xiaoqiang <zhengqianglong@zuoyebang.com>
2022-07-14 17:25:19 +0800

    添加文件

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,26 @@
 
 /// <reference types="ps-extendscript-types"/>
 
+export {Application, HostVersion } from "./lib/Application";
+export {Artboard } from "./lib/Artboard";
+export {Color} from "./lib/Color";
+export {Document} from "./lib/Document";
+export {History} from "./lib/History";
+export {Layer} from "./lib/Layer";
+export {MetaData} from "./lib/MetaData";
+export {Rect} from "./lib/Rect";
+export {Selection} from "./lib/Selection";
+export {Shape} from "./lib/Shape";
+export {Size} from "./lib/Size";
+export {Stroke, StrokeLineType} from "./lib/Stroke";
+
+export {Response} from "./util/Response";
+export {Rectangle} from "./lib/Shape";
+export {Console} from "./util/Console";
+export {Utils} from "./util/Utils";
+export {Kind} from "./util/Constants";
+
+/*
 import {Application as _Application} from "./lib/Application";
 import {Artboard as _Artboard} from "./lib/Artboard";
 import {Color as _Color} from "./lib/Color";
@@ -43,3 +63,5 @@ Shape = _Shape;
 Size = _Size;
 // @ts-ignore
 Stroke = _Stroke;
+
+ */
